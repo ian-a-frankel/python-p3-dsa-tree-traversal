6cc338526102967c6da3bce2a58d334dd8ad4736
Ian Frankel <ian.frankel@gmail.com>
2023-11-17 17:51:32 -0500

    depth first searched

--- lib/tree.py
+++ lib/tree.py
@@ -3,4 +3,12 @@ class Tree:
     self.root = root
 
   def get_element_by_id(self, id):
-    pass
+    found = None
+    if self.root['id'] == id:
+      found = self.root
+    else:
+      for child in self.root['children']:
+        child_tree = Tree(child)
+        print(child_tree)
+        return child_tree.get_element_by_id(id)
+    return found
